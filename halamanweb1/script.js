9a4716c1bc691a87914bbf889d31bf8117efd0ba
Devin Tanjungan <103755933+DevinTannn@users.noreply.github.com>
2026-04-15 11:31:39 +0700

    test 0.3
    ubah dikit

--- halamanweb1/script.js
+++ halamanweb1/script.js
@@ -1,18 +1,41 @@
-function halo(){
-  alert("Selamat datang di website kami!");
+window.addEventListener("scroll", function() {
+  let elements = document.querySelectorAll(".animasi");
+
+  elements.forEach(el => {
+    let posisi = el.getBoundingClientRect().top;
+    let tinggiLayar = window.innerHeight;
+
+    if (posisi < tinggiLayar - 100) {
+      el.classList.add("muncul");
+    }
+  });
+});
+
+/* tombol hero */
+function halo() {
+  alert("Selamat datang di Agro Sawit Nusantara!");
 }
 
-function kirim(){  
-  let nama = document.getElementById("nama").value;  
-  if(nama === ""){
-    alert("Nama harus diisi dulu!");
-    return;
+/* form */
+function kirim() {
+  let nama = document.getElementById("nama").value;
+
+  if (nama === "") {
+    alert("Nama harus diisi!");
+  } else {
+    alert("Pesan berhasil dikirim, " + nama);
+
+    // RESET FORM
+    document.getElementById("formKontak").reset();
   }
-
-  alert("Terima kasih, " + nama + "! Pesan berhasil dikirim!");  
-  document.getElementById("formKontak").reset(); 
 }
 
-function terms(){
-  alert("Konten dilindungi hak cipta. Dilarang menyalin tanpa izin.");
-}
+window.addEventListener("scroll", function() {
+  let navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
